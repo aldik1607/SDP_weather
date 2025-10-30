9e72d4dd278f834f03f53548c81f6b23a30558f7
Aldiyar <aldiyar.tandau2007@gmail.com>
2025-10-30 13:28:38 +0500

    fix

--- src/main/java/com/example/weather/controller/WeatherController.java
+++ src/main/java/com/example/weather/controller/WeatherController.java
@@ -5,6 +5,7 @@ import com.example.weather.facade.WeatherServiceFacade;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/weather")
 public class WeatherController {
